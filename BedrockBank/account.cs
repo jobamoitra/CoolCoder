03dc2bb7a3ae6e4042be2915b4304f08e87817ee
Joba Moitra <jobamoitra@hotmail.com>
2015-11-18 23:06:05 -0800

    Starting code

---
+++ BedrockBank/account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    /// <summary>
+    /// this class provides defination for a Bedrock account
+    /// </summary>
+    class account
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the account holder
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Account number
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Current balance in the account
+        /// </summary>
+        public decimal Balance { get; set; }
+        #endregion
+
+        #region
+        
+        #endregion
+    }
+}
